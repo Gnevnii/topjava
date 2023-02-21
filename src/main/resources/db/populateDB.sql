540b5a51b8de180c2276870ec902567265fcd70d
gnevnii <gnev@gnevnii.ru>
2023-02-21 15:58:01 +0400

    HWO3 - init and fill table meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -10,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+
+INSERT INTO meals (user_id, description, calories, datetime)
+VALUES (100000, 'Завтрак', 500, timestamp '2020-01-30 10:00'),
+       (100000, 'Обед', 1000, timestamp '2020-01-30 13:00'),
+       (100000, 'Ужин', 500, timestamp '2020-01-30 20:00'),
+       (100000, 'Еда на граничное время', 100, timestamp '2020-01-31 00:00'),
+       (100000, 'Завтрак', 1000, timestamp '2020-01-31 10:00'),
+       (100000, 'Ужин', 410,  timestamp '2020-01-31 20:00');
